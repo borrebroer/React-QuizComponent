f46451900c0bc4155d5072c85f913ccfd14822b0
Borre Broer <borre@use.startmail.com>
2021-09-18 23:35:44 +0200

    vierde commit

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,13 +6,12 @@ class QuizQuestion extends Component {
         return(
             <main>
             <section>
-              <p>// instruction text goes here
+              <p>
                   {this.props.instruction_text}
               </p>
             </section>
             <section className="buttons">
               <ul>
-                //quiz question button logic goes here
                 <li>{this.props.quiz_question.answer_options[0]}</li>
               </ul>
             </section>
