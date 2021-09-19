47a1049db95d1656f7a0aca430e18edcd5f22d27
Borre Broer <borre@use.startmail.com>
2021-09-19 22:01:54 +0200

    elfde commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json');
 
@@ -7,14 +8,26 @@ class Quiz extends Component {
 
     constructor(props){
         super(props);
-        this.state = {quiz_position: 1}
+        this.state = {quiz_position: 1};
     }    
 
 
     render(){
-        return(<div>
-            <QuizQuestion quiz_question={quizData.quiz_questions[0]} />
-        </div>)
+        const isQuizEnd = quizData.quiz_questions.length === (this.state.quiz_position - 1);
+        if(isQuizEnd){
+            return(
+                <div>
+                    <QuizEnd/>
+                </div>
+            );
+        }
+        else {
+            return(
+                <div>
+                    <QuizQuestion quiz_question={quizData.quiz_questions[0]} />
+                </div>
+            );    
+        }
     }
 }
 export default Quiz;
