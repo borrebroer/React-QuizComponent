cfda2d5c57c53dfac611d7c2e4f5460b5760c9b2
Borre Broer <borre@use.startmail.com>
2021-09-19 21:07:53 +0200

    tiende commit

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,7 +4,7 @@ import React, {Component} from "react";
 class QuizQuestionButton extends Component{
 render(){
     return(
-        <li><button text={this.props.button_text}></button></li>
+        <li><button>{this.props.button_text}</button></li>
     );
     }
 }
