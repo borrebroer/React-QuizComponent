52d3982d8baa58457145f4101a2753b13457ce2a
Borre Broer <borre@use.startmail.com>
2021-09-22 20:01:32 +0200

    zeventiende commit

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,12 +4,12 @@ import React, {Component} from "react";
 class QuizQuestionButton extends Component{
     render(){
         return(
-            <li><button onClick={this.handleClick.bind(this)} text={this.props.button_text}></button></li>
+            <li><button onClick={this.handleClick.bind(this)} >{this.props.button_text}</button></li>
         );
     }
 
     handleClick(){
-        this.props.clickHandler(button_text);
+        this.props.clickHandler(this.props.button_text);
     }
 }
 
